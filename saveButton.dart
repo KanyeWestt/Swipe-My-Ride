8e90a53e5e338e79ed02668dc9518ecfb06f31df
DTVizzle <55521080+DTVizzle@users.noreply.github.com>
2019-09-20 11:18:53 +1200

    Add files via upload

--- saveButton.dart
+++ saveButton.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:swipe_my_ride/record.dart';
+import 'user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class SaveButton extends StatelessWidget{
+class SaveButton extends StatelessWidget {
   final Record record;
   final User user;
-  SaveButton(this.record, this.user)
-  {
-//    this.record.description
-  }
+  SaveButton(this.record, this.user);
 
   @override
-  Widget build(BuildContext context){
-    Scaffold(
-
-    );
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: alreadySaved(user.name, record.getDocID()),
+        builder: (context, AsyncSnapshot<bool> result) {
+          if (!result.hasData)
+            return Container(
+              child: IconButton(icon: Icon(Icons.favorite), color: Colors.red),
+            );
+          else
+            return Container(
+                child: IconButton(
+                    icon: Icon(Icons.favorite_border),
+                    color: Colors.red,
+                    onPressed: () {
+//                      this.user.updateDB(this.record.getDocID());
+                      Icon(Icons.favorite);
+                    }));
+        });
   }
 
+  Future<bool> alreadySaved(String name, String documentID) async {
+    final QuerySnapshot result = await Firestore.instance
+        .collection('users')
+        .where('name', isEqualTo: name)
+        .getDocuments();
 
-
-}
-
-//Dummy user class for when creating a save button
-class User{
-  final String name;
-  final String age;
-  User (this.name, this.age);
+    return result.documents.contains(documentID);
+  }
 }
